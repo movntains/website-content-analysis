7be4abb65159ed5f0363214f2c5375cd94377855
Rachel Opperman <oppermanre17@gmail.com>
2025-09-30 17:45:14 -0400

    Initial commit

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+test('guests are redirected to the login page', function () {
+    $this->get(route('dashboard'))->assertRedirect(route('login'));
+});
+
+test('authenticated users can visit the dashboard', function () {
+    $this->actingAs($user = User::factory()->create());
+
+    $this->get(route('dashboard'))->assertOk();
+});
