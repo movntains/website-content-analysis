72aa57271432908a8b1302fdbd647b35c834e4d7
Rachel Opperman <oppermanre17@gmail.com>
2025-09-30 21:58:35 -0400

    Adds UserTokenLimit model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use App\Traits\HasUuid;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -16,31 +17,27 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasUuid, Notifiable, TwoFactorAuthenticatable;
 
-    /**
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tokenLimit(): HasOne
+    {
+        return $this->hasOne(UserTokenLimit::class);
     }
 }
---
+++ database/migrations/2025_10_01_011054_create_user_token_limits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user_token_limits', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->foreignIdFor(User::class)
+                ->unique()
+                ->constrained()
+                ->onDelete('cascade');
+            $table->integer('monthly_token_limit')->default(10000);
+            $table->integer('current_month_usage')->default(0);
+            $table->dateTime('last_reset_date')->default(now()->toDateString());
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('user_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('user_token_limits');
+    }
+};
