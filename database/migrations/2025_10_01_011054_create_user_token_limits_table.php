3aaca50ccbe4f8843c0d7af173499779bbd4cd7f
Rachel Opperman <oppermanre17@gmail.com>
2025-10-02 15:37:39 -0400

    Uses explicit column name for foreign key

--- database/migrations/2025_10_01_011054_create_user_token_limits_table.php
+++ database/migrations/2025_10_01_011054_create_user_token_limits_table.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('user_token_limits', function (Blueprint $table) {
             $table->id();
             $table->uuid()->unique();
-            $table->foreignIdFor(User::class)
+            $table->foreignId('user_id')
                 ->unique()
                 ->constrained()
                 ->onDelete('cascade');
