20f1824eb8b05f5bf435dfd872fa9b6349c373f6
Rachel Opperman <oppermanre17@gmail.com>
2025-10-02 17:06:48 -0400

    Removes individual usage of RefreshDatabase

--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
-
 test('password update page is displayed', function () {
     $user = User::factory()->create();
 
