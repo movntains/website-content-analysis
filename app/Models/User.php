7e36e6deeefd06139f40edef066864be4e766d9d
Rachel Opperman <oppermanre17@gmail.com>
2025-10-02 17:31:29 -0400

    Ensures email is set as all lowercase

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Scan::class);
     }
+
+    public function setEmailAttribute($value): void
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
 }
