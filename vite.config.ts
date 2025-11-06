f38fcd8ba206dc22a65d345d1f64653720bcb1ee
Rachel Opperman <oppermanre17@gmail.com>
2025-11-06 17:47:12 -0500

    feat: add DDEV for local development

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,13 @@ export default defineConfig({
   esbuild: {
     jsx: 'automatic',
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+    cors: {
+      origin: /https?:\/\/([A-Za-z0-9\-.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+  },
 });
